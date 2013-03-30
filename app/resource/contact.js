ffb14e20d514feca0f6e1302f7b169a55ca1d42a
John Smith <john.smith.17th@gmail.com>
2013-03-30 14:34:59 +0800

    Fix get contact and blacklist problem, return more user information

--- app/resource/contact.js
+++ app/resource/contact.js
@@ -1,7 +1,27 @@
 var servs = require('../../servs');
 
 function getContact(session, params, callback) {
-    servs.d('/contact', 'get', { uid: session.uid, black: false }, callback);
+    servs.d('/contact', 'get', { uid: session.uid, black: false }, function (e, r) {
+        if (r && r.length) {
+            var map = {}, uid = [], x;
+            for (var i in r) {
+                x = r[i];
+                if (x.alias) map[cid] = x.alias;
+                uid.push(x.cid);
+            }
+            servs.d('/user/q', 'post', { uid: uid }, function (e1, r1) {
+                if (r1) {
+                    r1.forEach(function (obj, i, arr) {
+                        arr[i] = obj.toObject();
+                        x = arr[i];
+                        if (map[x.uid]) x.alias = map[x.uid];
+                    });
+                }
+                callback(e1, r1);
+            });
+        }
+        else callback(e, r);
+    });
 }
 
 function putContact(session, params, callback) {
