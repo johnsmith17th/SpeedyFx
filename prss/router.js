02bfb21447befb31fcedc5739aae280e9ea66ec8
John Smith <john.smith.17th@gmail.com>
2013-03-30 11:45:50 +0800

    Modify router of presence service
    - method delete equals del

--- prss/router.js
+++ prss/router.js
@@ -65,6 +65,9 @@ var route = {
 };
 
 var router = module.exports = function (res, method, params, callback) {
+
+    if (method == 'delete') method = 'del';
+
     if (route[res] && route[res][method])
         route[res][method](params, callback);
     else callback();
