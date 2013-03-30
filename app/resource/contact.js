8ec8d4a673855827289db4d025b628f7f93ab55a
John Smith <john.smith.17th@gmail.com>
2013-03-30 12:01:38 +0800

    Contact handler of application service

--- app/resource/contact.js
+++ app/resource/contact.js
@@ -1,1 +1,21 @@
 var servs = require('../../servs');
+
+function getContact(session, params, callback) {
+    servs.d('/contact', 'get', { uid: session.uid, black: false }, callback);
+}
+
+function putContact(session, params, callback) {
+    var p = { uid: session.uid, cid: params.cid };
+    if (params.alias) p.alias = params.alias;
+    servs.d('/contact', 'put', p, callback);
+}
+
+function delContact(session, params, callback) {
+    servs.d('/contact', 'delete', { uid: session.uid, cid: params.cid }, callback);
+}
+
+module.exports = {
+    get: getContact,
+    put: putContact,
+    del: delContact
+};
